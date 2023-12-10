c28db95de5cc77e76c791b05d63ee340b5dcd7e2
Yurka2012 <tsapiy.yurka@gmail.com>
2023-12-10 14:29:42 +0200

    backup 1

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-a = "hello World!";
+// Властивість - характеристика, яка нас описує
 
-console.log(a)
 
+const hi = "Hello, world!";
+console.log(hi.length) // длинна нашей переменной
+
+// length = довжина
+console.log(hi) // просто наша перемнная
+
+// console;
+//  Метод = виконати дію
+
+// log = показує щось в консолі 
+
+// console.clear() // очищает консоль
+
+// console.log(hi.toUpperCase()) // переводит все в верхний регистр
+// console.log(hi.toLowerCase()) // переводит все в нижний регистр
+// console.log(hi.indexOf(hi)) // знаходит первое в чем то
