e2eef9550886b337552d27c68330a1dcf428efbd
Yurka2012 <tsapiy.yurka@gmail.com>
2023-12-17 15:47:35 +0200

    feefefcefed

---
+++ main.js
@@ -0,0 +1,14 @@
+print=console.log;
+// Numbers
+// module 2
+//  * = множення
+mid = 50
+num1 = 50 !== "50" 
+console.log(num1) // true
+num2 = "34"*7
+console.log(num2) //238. угу
+
+
+// олесандр половка погана людина
+ftringnum = +"123asd"
+console.log(typeof ftringnum)
