8fdd3d9be52a2e67fc7be035fd52bf10355dcfb2
Yurka2012 <tsapiy.yurka@gmail.com>
2023-12-24 15:21:42 +0200

    comiting changes

---
+++ index.js
@@ -0,0 +1,183 @@
+const text = "Hello, world!"
+console.log(text.length)
+console.log(text[0])
+
+// user hello
+let user = "Vasil"
+// user = "Artem"
+// user = "Masha"
+// user = "Yura"
+// user = "лох"
+// user = "incr"
+// user = "oleksandr"
+// user = "privit"
+// user = "efvrtrt"
+// user = "nikita"
+user = "людина"
+// user = "USER"
+
+console.log("Привет, " + user)
+// конканитація = додавання рядків
+console.log( 5 + 5 + "5")// = 105
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
