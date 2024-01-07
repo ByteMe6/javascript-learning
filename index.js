80a4d58c1e66ad512698f8649c3c065d6fb1582d
Yurka2012 <tsapiy.yurka@gmail.com>
2024-01-07 14:05:21 +0200

    Not END

---
+++ index.js
@@ -0,0 +1,17 @@
+// let notnum = "123" 
+// let num = parseInt(notnum)  
+// console.log(notnum) 
+// console.log(num) 
+//  все що можна зробити щоб стірнги були інтенгара
+// дом - це потрібна фігня
+const user = { 
+    name: 'Ivan', 
+    age: 25,  
+    city: 'Kyiv' 
+}
+console.log(user) 
+
+
+console.clear()
+
+// const b = let a = "vasil"
