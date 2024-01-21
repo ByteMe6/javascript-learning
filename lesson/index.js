f969f6dedb03e237f3366a2dab69bdc52a8a644b
Yurka2012 <tsapiy.yurka@gmail.com>
2024-01-21 19:38:16 +0200

    bugofix

--- lesson/index.js
+++ lesson/index.js
@@ -1,18 +1,46 @@
 // || - або
 // ! = не
 // && = І
+console.clear;
 // !true = false
 // !false = true
 // true = true
 // false = false
 
-console.log(umbrella() && goodWeather())
+// console.log(umbrella() && goodWeather())
 
-function umbrella() {
-  console.log("Парасолька");
-}
-function goodWeather() {
-  console.log("Гарна погода");
-}
+// function umbrella() {
+//   console.log("Парасолька");
+// }
+// function goodWeather() {
+//   console.log("Гарна погода");
+// }
 
-console.log(umbrella() && goodWeather())
+// if ("artem", "", false) {
+//   console.log("Правда")
+// } else {
+//   console.log("Фальшивка")
+//   // console.log(false)
+// }
+
+let user = 5000000000
+
+const lochSub = 200
+const ultra = 500
+const MEGASUPERULTRA = 10000
+if (user >= lochSub){
+  console.clear;
+ console.log("ви можете купити лох підписку")
+}
+if (user >= ultra){
+  // console.clear;
+ console.log("ви можете купити нормальну підписку")
+}
+if (user >= MEGASUPERULTRA){
+  // console.clear;
+ console.log("ви можете купити гарну підписку")
+}
+if (user < lochSub)  {
+  // console.clear;
+  console.log(" ви хуже лоха. ви бомж")
+}
