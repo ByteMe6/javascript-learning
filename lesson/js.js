1534d342ec385ac9084a6b76c861a1b95b2ffe95
Yurka2012 <tsapiy.yurka@gmail.com>
2024-01-28 16:04:33 +0200

    /* Тернарні оператори */ let age = 17;  const isAdult = age >= 18 ? "Adult" : "Not adult"; console.log(isAdult) // тернарний опратор - річ для для найпростіших порівнянь if  (age >= 18) { console.log("adult")}else{console.log("children")} // карочє нікому нє нужна фігня.

---
+++ lesson/js.js
@@ -0,0 +1,8 @@
+/* Тернарні оператори */
+let age = 17;
+
+const isAdult = age >= 18 ? "Adult" : "Not adult";
+console.log(isAdult)
+// тернарний опратор - річ для для найпростіших порівнянь
+if  (age >= 18) { console.log("adult")}else{console.log("children")}
+// карочє нікому нє нужна фігня.
