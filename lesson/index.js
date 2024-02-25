d69eb3978d76cb5753a0f1d0251fde81265bb977
Yurka2012 <tsapiy.yurka@gmail.com>
2024-02-25 18:01:51 +0200

    remake

--- lesson/index.js
+++ lesson/index.js
@@ -1,11 +1,11 @@
 // fffffffffff
-// fff        
 // fff
-// ffffffffff        
-// fff       
-// fff        
-// fff        
-// fff        
+// fff
+// ffffffffff
+// fff
+// fff
+// fff
+// fff
 
 // uuuu        uuuu
 // uuuu        uuuu
@@ -14,11 +14,11 @@
 // uuuu        uuuu
 // uuuu        uuuu
 // uuuu        uuuu
-//  uuuu      uuuu 
-//    uuuuuuuu     
+//  uuuu      uuuu
+//    uuuuuuuu
 
-//     nnnnnnnn   
-//   nnnn    nnnn 
+//     nnnnnnnn
+//   nnnn    nnnn
 //  nnnn      nnnn
 //  nnnn      nnnn
 //  nnnn      nnnn
@@ -28,25 +28,24 @@
 //  nnnn      nnnn
 
 //        ccccccccccc
-//     ccccccc       
-//   ccccccc         
-//  ccccccc          
-//  ccccccc          
-//  ccccccc          
-//    ccccccc        
-//     ccccccc       
+//     ccccccc
+//   ccccccc
+//  ccccccc
+//  ccccccc
+//  ccccccc
+//    ccccccc
+//     ccccccc
 //        ccccccccccc
 
-//   tttt       
-//   tttt       
-//   tttt       
-//   tttttttt 
-//   tttttttt   
-//   tttt       
-//   tttt       
-//   tttt  tttt 
-//   tttttttt   
-
+//   tttt
+//   tttt
+//   tttt
+//   tttttttt
+//   tttttttt
+//   tttt
+//   tttt
+//   tttt  tttt
+//   tttttttt
 
 //    iii
 //   iiiii
@@ -57,15 +56,24 @@
 //  iiiiiiii
 //  iiiiiiii
 //  iiiiiiii
+//  iiiiiiii
+//  iiiiiiii
+//  iiiiiiii
 
-//     ooooooooo
-//  ooooo     ooooo   
-// ooooo       ooooo
-//  ooooo     ooooo  
-//     oooooooooo    
 
-//     nnnnnnnn   
-//  nnnn    nnnn 
+//      ooooooooo
+//      ooooooooo
+//  ooooo       ooooo
+//  ooooo       ooooo
+// ooooo         ooooo
+// ooooo         ooooo
+//  ooooo       ooooo
+//  ooooo       ooooo
+//      oooooooooo
+//      oooooooooo
+
+//     nnnnnnnn
+//  nnnn    nnnn
 // nnnn      nnnn
 // nnnn      nnnn
 // nnnn      nnnn
@@ -73,3 +81,18 @@
 // nnnn      nnnn
 // nnnn      nnnn
 // nnnn      nnnn
+
+function doCoffee(water, milk, coffee) {
+  console.log("вода : " + water, "молока : " + milk, "кава : " + coffee);
+
+  if (coffee && milk && water) {
+    console.log("Роблю каву з молоком");
+  } else if (water && coffee) {
+    console.log("Роблю каву ");
+  } else if (water) {
+    console.log("Додайте каву та молоко бо є лише вода");
+  } else {
+    console.log("тут нічого немає");
+  }
+}
+a = 1 //same as let
