2559ad90de98d8beb175c6c4493673879f30f830
Yurka2012 <tsapiy.yurka@gmail.com>
2024-02-25 15:25:29 +0200

    big text

--- lesson/index.js
+++ lesson/index.js
@@ -1,34 +1,12 @@
-// Вивчимо методи масивів
-// Навчимося робити з рядка масив та навпаки(split,join)
-// Перевірки на існування елементу в масиві (indexOf , includes)
-// Дізнаємося як можна додати елементи в масив та вирізати або видалити один або декілька елементів.(shift,unshift,pop,push,slice,splice)
-
-const { concat } = require("lodash");
-
-// Навчимося об’єднувати декілька масивів в один (concat)
-const num1 = [1, 2, 3, 4, 5];
-const num2 = [6, 7, 8, 9, 10];
-const num3 = [11, 12, 13, 14, 15];
-const num12 = num1.concat(num1);
-const num123 = [...num1, ...num2, ...num3];
-console.log(num12);
-console.log(num123);
-
-const myStudents = ["Yura", "Oleksandr", "Artem", "Vladislav", "Mykyta"]
-const tupiStudents = ["Tupiy Vasya", "Tupiy Kolya", "Tupiy debil"]
-const iTupiINormalniStudents = myStudents.concat(tupiStudents);
+// function
+fffffffffff     uuuu        uuuu       nnnnnnnn            ccccccccccc    tttt            iii
+fffffffffff     uuuu        uuuu     nnnn    nnnn       ccccccc           tttt           iiiii
+fff             uuuu        uuuu    nnnn      nnnn    ccccccc             tttt            iii
+ffff            uuuu        uuuu    nnnn      nnnn   ccccccc              tttttttt 
+ffff            uuuu        uuuu    nnnn      nnnn   ccccccc              tttttttt      iiiiiiii
+ffffffffff      uuuu        uuuu    nnnn      nnnn   ccccccc              tttt          iiiiiiii
+fff             uuuu        uuuu    nnnn      nnnn     ccccccc            tttt          iiiiiiii
+fff              uuuu      uuuu     nnnn      nnnn      ccccccc           tttt  tttt    iiiiiiii
+fff                uuuuuuuu         nnnn      nnnn         ccccccccccc    tttttttt      iiiiiiii
 
 
-console.log(myStudents)
-console.log(tupiStudents)
-console.log(myStudents + tupiStudents)
-console.log(iTupiINormalniStudents)
-
-const stringi = "Hello, world!"
-const  arrayi = stringi.split(",")
-console.log(arrayi)
-const joinMe = arrayi.join("")
-console.log(arrayi.join(", stupid"))
-const str11 = ["Hello World", "hello world"]
-str11[0] = str11[0].split("")
-console.log(str11)
