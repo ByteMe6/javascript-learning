8d3b4f7e36f153cc748f02145fae8bf032ef957d
Yurka2012 <tsapiy.yurka@gmail.com>
2024-03-03 14:10:28 +0200

    по рукам бить иишку

--- hw/main.js
+++ hw/main.js
@@ -1,3 +1,4 @@
+// Я нифига не понял. по рукам бить ии
 // Задание 1
 
 // Напиши функцию logItems(array), которая получает массив и использует цикл for, который для каждого элемента массива будет выводить в консоль сообщение в формате [номер элемента] - [значение элемента].
@@ -163,33 +164,33 @@ if (numbers.length !== 0) {
 
 
 
-const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
-const isLoginValid = function(login) {
-  return login.length >= 4 && login.length <= 16;  
-};
+// const isLoginValid = function(login) {
+//   return login.length >= 4 && login.length <= 16;  
+// };
 
-const isLoginUnique = function(allLogins, login) {
-  return !allLogins.includes(login);
-};
+// const isLoginUnique = function(allLogins, login) {
+//   return !allLogins.includes(login);
+// };
 
-const addLogin = function(allLogins, login) {
-  if (!isLoginValid(login)) {
-    return 'Ошибка! Логин должен быть от 4 до 16 символов';
-  }
+// const addLogin = function(allLogins, login) {
+//   if (!isLoginValid(login)) {
+//     return 'Ошибка! Логин должен быть от 4 до 16 символов';
+//   }
   
-  if (!isLoginUnique(allLogins, login)) {
-    return 'Такой логин уже используется!';  
-  }
+//   if (!isLoginUnique(allLogins, login)) {
+//     return 'Такой логин уже используется!';  
+//   }
 
-  allLogins.push(login);
-  return 'Логин успешно добавлен!';  
-};
+//   allLogins.push(login);
+//   return 'Логин успешно добавлен!';  
+// };
 
-console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+// console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
 
-console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+// console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
 
-console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+// console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
 
-console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+// console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
